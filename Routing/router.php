d32ef9f8e756d008d5d571fc2550a7a1bb68f9b2
Admin <Admin>
2017-04-26 10:27:55 +0300

    Added diving

--- Routing/router.php
+++ Routing/router.php
@@ -9,7 +9,28 @@
 namespace Routing;
 
 
-class router
+class UrlMatcher
 {
+    private $methods=array(
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+        'HEAD'
+    );
+    private $routers =array(
+        'GET'=>array(),
+        'POST'=>array(),
+        'PUT'=>array(),
+        'DELETE'=>array(),
+        'PATCH'=>array(),
+        'HEAD'=>array()
+    );
+
+    private $patterns=array(
+        'num'=>'[0-9]+',
+        'str'=>'[a-zA-Z\.\-_%]+',
+        'any'=>'[a-zA-Z0-9\.\-_%]+'
+    );
 
 }
